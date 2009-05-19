f20b236f6edc1b44e1fc4dcf70bd9f7cfd20f3be
Sheldon Finlay <jagfly@gmail.com>
2009-05-18 21:42:43 -0400

    added attachable_type= method to attaching.rb enabling support for STI models.

--- lib/attaching.rb
+++ lib/attaching.rb
@@ -2,6 +2,10 @@ class Attaching < ActiveRecord::Base
   belongs_to :asset, :counter_cache => true
   belongs_to :attachable, :polymorphic => true
   
+  def attachable_type=(type)
+     super(type.to_s.classify.constantize.base_class.to_s)
+  end
+  
   def after_create
     if self.attachable.acts_as_polymorphic_paperclip_options[:counter_cache]
       self.attachable.class.increment_counter(:assets_count, self.attachable.id)
